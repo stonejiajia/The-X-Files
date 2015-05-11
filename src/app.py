7f9a4d30483c2764b792ac4b6148c4b0cee37e8f
cici1989 <houjine@126.com>
2015-05-11 22:12:52 +0800

    app

--- src/app.py
+++ src/app.py
@@ -1,9 +1,15 @@
-# test for bottle
+# -*- coding: utf-8 -*-
 
-from bottle import route, run
+from bottle import route,run
 
 @route('/')
+@route('/index.html')
 def index():
-    return 'Hello World!'
+    return '<a href="/hello">书单排行表</a>'
+#return '<a href="/hello">本周排行表</a>'
+
+@route('/hello')
+def hello():
+    return '柱状图'
 
 run(host='localhost', port=8080)
