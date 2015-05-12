6d1828522128a3285c76e1ab96e1216b95cab59f
cici1989 <houjine@126.com>
2015-05-12 22:32:56 +0800

    app.py

--- src/app.py
+++ src/app.py
@@ -5,11 +5,16 @@ from bottle import route,run
 @route('/')
 @route('/index.html')
 def index():
-    return '<a href="/hello">书单排行表</a>'
-#return '<a href="/hello">本周排行表</a>'
+    return '''<a href="/chart">书单排行表</a>
+        
+        <a href="/chart_week">本周排行表</a>'''
 
-@route('/hello')
-def hello():
-    return '柱状图'
+@route('/chart')
+def chart():
+    return '总书单柱状图'
+
+@route('/chart_week')
+def chart_week():
+    return '本周柱状图'
 
 run(host='localhost', port=8080)
