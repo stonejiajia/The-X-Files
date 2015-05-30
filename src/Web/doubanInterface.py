eb8a2abe66811c983a129b806f01414aaa6c53f8
Max Wang <wangppei@gmail.com>
2015-05-30 14:18:57 +0800

    #update# get_member_booklist() is ready.

--- src/Web/doubanInterface.py
+++ src/Web/doubanInterface.py
@@ -25,11 +25,29 @@ class bookIf:
             else:
                 return self.__get_douban_booklist(self.author_booklist_url)
             
-    def __get_douban_booklist(self, url):          
-        # use urllib to get html data from url
+
+
+    def get_member_booklist(self, member_id, list_type = 'wish'):
+        root_url = "http://book.douban.com/people/" + member_id
+        if list_type == "wish":
+            full_url = root_url + "/wish"
+            return self.__get_douban_people_readlist(full_url, False) # for wish list, book rating is meaningless, False for not get rating.
+        elif list_type == "reading":
+            full_url = root_url + "/do"
+            return self.__get_douban_people_readlist(full_url)
+        elif list_type == "done":
+            full_url = root_url + "/collect"
+            return self.__get_douban_people_readlist(full_url)
+        else:
+            return None
+
+    def get_book_info(self, book_id):
+        pass
+    
+    def __get_douban_booklist(self, url):
         item = 0
         total_book_list = []
-        while 1:
+        while True:
             full_url = url + "?start={0}&sort=time".format(item)
             response = urllib2.urlopen(full_url)
             #print "=============================================read page " + str(item/25+1) + "==================================================="
@@ -41,10 +59,8 @@ class bookIf:
             #striped_html = raw_html.strip('\n')
             #striped_html = striped_html.strip('\r')
             #striped_html = striped_html.strip('\n')
-            #print oneline_html_text
             html_text_list = html_text.splitlines()
-            #print "==========================================================raw and one line========================================================="
-            #print html_text_list
+
             book_list = []
             for i in range(0, len(html_text_list)):
                 if html_text_list[i].find('<div class="title">') >= 0:
@@ -58,20 +74,59 @@ class bookIf:
                         book_info['book_name'] = book_name
                         #print book_info
                         book_list.append(book_info)
-                 
-            #print "==========================================================whole book_list========================================================="        
+                       
             if book_list:
                 #print book_list
                 total_book_list += book_list
                 item += 25
-                time.sleep(2)
+                time.sleep(1) #sleep 1 second to avoid being blocked by douban.com for frequent access.
             else:
                 break
         #print total_book_list
         return total_book_list
+    
+    def __get_douban_people_readlist(self, url, WANT_RATING = True):
+        item_num = 0
+        total_book_list = []
+        
+        while True:
+            #print "=============================================read page " + str(item_num/15+1) + "==================================================="
+            full_url = url + "?start={0}&sort=time".format(item_num)
+            response = urllib2.urlopen(full_url)
+            html_text = response.read()
+            html_text_list = html_text.splitlines()
 
-    def get_member_booklist(self, member_id, list_type = 'desire'):
-        pass
+            book_list = []
+            got_book_title = False    # used for search book rating. only after a book title is found, book rating is to be searched.
+            
+            for html_line in html_text_list:
+                book_info = {"book_id":"", "book_name":"", "rating":0}
+                
+                if WANT_RATING and got_book_title:
+                    rate_search = re.search('(<span class="rating)(\d)(-t"></span>)', html_line)
+                    if rate_search:
+                        book_rating = rate_search.group(2)
+                        book_list[len(book_list)-1]["rating"] = int(book_rating)
+                        got_book_title = False
+                        continue
 
-    def get_book_info(self, book_id):
-        pass
+                search_result = re.search('(http://book.douban.com/subject/)(\d+)(/)(.+)(title=")(.+)(")', html_line)
+                if search_result:
+                    book_id = search_result.group(2)
+                    book_name = search_result.group(6)
+                    
+                    book_info['book_id'] = book_id
+                    book_info['book_name'] = book_name
+                                    
+                    book_list.append(book_info)                        
+                    got_book_title = True
+                        
+            if book_list:
+                total_book_list += book_list
+                item_num += 15
+                print book_list
+                time.sleep(1) #sleep 1 second to avoid being blocked by douban.com for frequent access.
+            else:
+                break        
+        return total_book_list
+
